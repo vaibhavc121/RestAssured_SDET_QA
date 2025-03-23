39b12db7130d5592578183d67d4bb609fabee09e
vaibhav <vaibhavc121@gmail.com>
2025-03-23 23:41:39 +0530

    23-3

---
+++ src/test/java/day1_HTTPRequests/HTTPRequests.java
@@ -0,0 +1,15 @@
+package day1_HTTPRequests;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.Test;
+
+public class HTTPRequests
+{
+	@Test
+	public void getUser()
+	{
+		given().when().then();
+	}
+
+}
